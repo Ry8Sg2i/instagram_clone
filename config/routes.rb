1fd5d83a17e82cbc473ddaf08ef0e2367dbcf0d6
Ryu <azatose8684@gmail.com>
2023-09-11 14:07:40 +0900

    routes plus

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :posts, only: %i[index show new create destroy]
+  resources :posts, only: %i[index show new create destroy] do
+    scope module: :posts do
+      resource :likes, only: %i[create destroy]
+      resources :comments, only: %i[new create]
+    end
+  end
   resources :home, only: :index
   resource :profile, only: %i[show edit]
   root 'home#index'
