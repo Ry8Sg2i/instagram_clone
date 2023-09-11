b0ab77b33b780da2e92eca45e9727b5c14637132
Ryu <azatose8684@gmail.com>
2023-09-11 14:11:28 +0900

    post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,21 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   has_one_attached :thumbnail
   validate :thumbnail_presence
+  
+  def liked?(current_user)
+    likes.where(user: current_user).exists?
+  end
   private
 
   def thumbnail_presence
     errors.add(:thumbnail, 'must be attached') unless thumbnail.attached?
+  end
+
+  def show
+    @comment = Comment.new
   end
 end
